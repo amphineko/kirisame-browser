f47978362b73b159416bb811e9e7d0bf8e3a076c
amphineko <sukareki@gmail.com>
2019-10-10 14:58:55 +0800

    reduce AppMain useEffect dependency

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,31 @@
+import { AssertionError } from 'assert'
 import React, { useState, useEffect } from 'react'
 
 import { CameraPlayer } from './camera/player'
 import { DeviceSelector } from './camera/device_selector'
-import { AppStateDisplay, AppState } from './utils/state'
-// import { WebSocketSession } from './utils/push_session'
-
-import './app.css'
+import { Configuration, CaptureOptions } from './config'
 import { ImageSession } from './utils/image_session'
 import { ImageService } from './utils/image'
-import { Configuration } from './config'
+import { AppStateDisplay, AppState } from './utils/state'
+
+import './app.css'
+
+function configureDevice(config: CaptureOptions, deviceId?: string): Promise<MediaStream> {
+    const constrians = {
+        audio: false,
+        video: {
+            frameRate: config.frameRate,
+            height: config.height,
+            width: config.width,
+        },
+    } as MediaStreamConstraints
+
+    if (typeof deviceId === 'string' && deviceId.length > 0) {
+        (constrians.video as MediaTrackConstraints).deviceId = deviceId
+    }
+
+    return navigator.mediaDevices.getUserMedia(constrians)
+}
 
 export function AppMain(props: {
     config: Configuration
@@ -16,37 +33,31 @@ export function AppMain(props: {
     const [stream, setStream] = useState<MediaStream>(undefined)
     const [appState, setAppState] = useState<AppState>('init')
 
-    const videoConstriants = stream instanceof MediaStream
-        ? stream.getVideoTracks()[0].getConstraints() : undefined
+    const videoConstriants = stream.getVideoTracks()[0].getConstraints()
 
     const currentDeviceId = videoConstriants && videoConstriants.deviceId
         ? videoConstriants.deviceId[0] : undefined
 
-    function configureDevice(deviceId: string) {
-        const constrians = {
-            audio: false,
-            video: {
-                frameRate: props.config.capture.frameRate,
-                height: props.config.capture.height,
-                width: props.config.capture.width,
-            },
-        } as MediaStreamConstraints
-
-        if (typeof deviceId === 'string' && deviceId.length > 0) {
-            (constrians.video as MediaTrackConstraints).deviceId = deviceId
-        }
-
-        navigator.mediaDevices.getUserMedia(constrians).then((media) => {
-            setStream(media)
+    function updateDeviceId(deviceId?: string) {
+        configureDevice(props.config.capture, deviceId).then((newStream) => {
+            if (newStream !== stream) { // prevent unnecessary update
+                if (stream.getVideoTracks().length === 0) {
+                    throw new AssertionError({ message: 'Stream should has at lease one video track' })
+                }
+                setStream(newStream)
+            }
         }).catch((error) => {
             setStream(undefined)
-            console.error(`Cannot start user device: ${error}`)
+            console.error(`Cannot start user device: ${{
+                deviceId: deviceId,
+                error: error,
+            }}`)
         })
     }
 
     if (stream === undefined) {
         // initialize default device
-        configureDevice(undefined)
+        updateDeviceId(undefined)
     }
 
     useEffect(() => {
@@ -54,7 +65,7 @@ export function AppMain(props: {
         const imageService = new ImageService(props.config.image)
         imageService.setVideoSourceObject(stream)
 
-        if (videoConstriants) { // has active stream
+        if (stream) { // has active stream
             imageClient = new ImageSession(props.config.image)
             imageClient.onclose = () => setAppState('retry')
             imageClient.onopen = () => setAppState('connected')
@@ -72,54 +83,7 @@ export function AppMain(props: {
                 imageClient.shutdown()
             }
         }
-
-        // let pushClient: WebSocketSession = undefined
-        // let recorder: MediaRecorder
-
-        // if (videoConstriants) {
-        //     pushClient = new WebSocketSession({
-        //         metadata: {
-        //             camera: {
-        //                 frameRate: Math.floor(videoConstriants.frameRate[0]),
-        //                 height: videoConstriants.height[0],
-        //                 width: videoConstriants.width[0]
-        //             },
-        //             video: {
-        //                 bitRate: props.config.streaming.bitRate,
-        //                 container: props.config.streaming.mime
-        //             }
-        //         },
-        //         retry: props.config.streaming.retry,
-        //         url: props.config.streaming.url
-        //     })
-
-        //     pushClient.events.addEventListener('connect', () => setAppState('connecting'))
-        //     pushClient.events.addEventListener('handshake', () => setAppState('handshake'))
-        //     pushClient.events.addEventListener('open', () => setAppState('connected'))
-        //     pushClient.events.addEventListener('close', () => setAppState('retry'))
-
-        //     const recorder = new MediaRecorder(stream, {
-        //         bitsPerSecond: props.config.streaming.bitRate,
-        //         mimeType: props.config.streaming.mime
-        //     })
-        //     recorder.ondataavailable = (event) => {
-        //         const reader = new FileReader()
-        //         reader.readAsArrayBuffer(event.data)
-        //         reader.onload = (event) => {
-        //             pushClient.send(event.target.result as ArrayBuffer)
-        //         }
-        //     }
-        //     recorder.start(1000)
-        //     console.log(recorder.state)
-        // }
-
-        // return () => {
-        //     if (pushClient)
-        //         pushClient.shutdown()
-        //     if (recorder)
-        //         recorder.stop()
-        // }
-    }, [stream, videoConstriants, props])
+    }, [stream, props])
 
     return (
         <div className="app-container">
@@ -129,8 +93,8 @@ export function AppMain(props: {
 
             <DeviceSelector
                 currentDeviceId={currentDeviceId}
-                onDeviceSelected={configureDevice}
-                onStop={() => setStream(null)}
+                onDeviceSelected={(deviceId) => updateDeviceId(deviceId)}
+                onStop={() => setStream(undefined)}
             />
         </div>
     )
